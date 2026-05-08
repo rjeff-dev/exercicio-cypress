a78f5d58c5097323db3476e9253c5c44a874951e
rjeff-dev <jeff_xd00@hotmail.com>
2026-05-08 14:05:25 -0300

    vqv

--- cypress/e2e/agendaDeContatos/AgC.cy.js
+++ cypress/e2e/agendaDeContatos/AgC.cy.js
@@ -1,31 +1,79 @@
 describe('Agenda de contatos', () => {
+
   beforeEach(() => {
     cy.visit('https://ebac-agenda-contatos-tan.vercel.app/')
   })
 
-    it('deve adicionar contato', () => {
-      cy.get('input[placeholder="Nome"]').type("Roberto Jefferson")
-      cy.get('input[placeholder="E-mail"]').type("jeff_xd00@hotmail.com")
-      cy.get('input[placeholder="Telefone"]').type("85991199258")
+  it('deve adicionar contato', () => {
 
-      cy.get('button[type="submit"]').click()
+    const nome = `Roberto ${Date.now()}`
+    const email = `teste${Date.now()}@mail.com`
+    const telefone = '85999999999'
 
-      cy.contains('Jeff').should('exist')
-    })
+    cy.get('input[placeholder="Nome"]').type(nome)
+    cy.get('input[placeholder="E-mail"]').type(email)
+    cy.get('input[placeholder="Telefone"]').type(telefone)
+
+    cy.get('button[type="submit"]').click()
+
+    cy.contains(nome)
+      .parents('.contato')
+      .within(() => {
+        cy.contains(nome).should('exist')
+        cy.contains(email).should('exist')
+        cy.contains(telefone).should('exist')
+      })
+  })
 
   it('deve editar um contato', () => {
-    cy.contains('Roberto Jefferson')
-        .parents('.contato')
-        .find('.edit')
-        .click()
-    })
 
-  describe('Remoção', () => {
-    it('deve remover contato', () => {
-      cy.contains('Roberto Jefferson')
+    const nome = `Roberto ${Date.now()}`
+    const nomeEditado = `Jefferson ${Date.now()}`
+    const email = `teste${Date.now()}@mail.com`
+    const telefone = '85999999999'
+
+    cy.get('input[placeholder="Nome"]').type(nome)
+    cy.get('input[placeholder="E-mail"]').type(email)
+    cy.get('input[placeholder="Telefone"]').type(telefone)
+    cy.get('button[type="submit"]').click()
+
+    cy.contains(nome)
+      .parents('.contato')
+      .find('.edit')
+      .click()
+
+    cy.get('input[placeholder="Nome"]')
+      .clear()
+      .type(nomeEditado)
+
+    cy.get('button[type="submit"]').click()
+
+    cy.contains(nomeEditado)
+      .parents('.contato')
+      .within(() => {
+        cy.contains(nomeEditado).should('exist')
+        cy.contains(email).should('exist')
+      })
+  })
+
+  it('deve remover um contato', () => {
+
+    const nome = `Roberto ${Date.now()}`
+    const email = `teste${Date.now()}@mail.com`
+    const telefone = '85999999999'
+
+    // cria
+    cy.get('input[placeholder="Nome"]').type(nome)
+    cy.get('input[placeholder="E-mail"]').type(email)
+    cy.get('input[placeholder="Telefone"]').type(telefone)
+    cy.get('button[type="submit"]').click()
+
+    cy.contains(nome)
       .parents('.contato')
       .find('.delete')
       .click()
-      })
-    })
+
+    cy.contains(nome).should('not.exist')
   })
+
+})
